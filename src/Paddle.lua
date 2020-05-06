6fb183224431b2129e3050f77c994421b188a287
Anderson Leonardo <andsnleo@gmail.com>
2020-05-06 18:43:59 -0300

    Create Paddle entity with horizontal moving behaviour

---
+++ src/Paddle.lua
@@ -0,0 +1,46 @@
+local K = love.keyboard
+local assetsLoader = gAssetsLoader
+local class = gClass
+local iffy = gIffy
+local push = gPush
+
+local this = class('Paddle')
+
+this.static.width = 64
+this.static.height = 16
+
+function this:initialize(x, y, opts)
+  opts = opts or {}
+
+  self.dx = 200
+  self.x = x or 0
+  self.y = y or 0
+  self.sx = opts.sx or 1
+  self.sy = self.sx
+  self.spriteName = 'bluePaddle'
+end
+
+function this:update(dt)
+  -- Move right
+  if K.isDown('right') then
+    self.x = self.x + self.dx * dt
+  end
+
+  -- Move left
+  if K.isDown('left') then
+    self.x = self.x - self.dx * dt
+  end
+
+  -- Collision with game window
+  if self.x <= 0 then
+    self.x = 0
+  elseif self.x + this.width >= push:getWidth() then
+    self.x = push:getWidth() - this.width
+  end
+end
+
+function this:render()
+  iffy.drawSprite(self.spriteName, self.x, self.y, 0, self.sx)
+end
+
+return this
